04e33668d537d3a9ea445e6c045d9de1464dfefe
Aykut Şahin <sahinaykut88@gmail.com>
2024-02-22 19:22:39 +0300

    Kodlama.io

---
+++ Business/Concrete/PttManager.cs
@@ -0,0 +1,34 @@
+﻿using Business.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class PttManager : ISupplierService
+    {
+        //ihtiyaç duyuduğum sınıfı newlemek yerine onun interface ini yazıyorum.
+
+        private IApplicantService _applicantService;
+        //classın başlaması için constructarını yazıyorum.newlendiğinde çalışır
+        public PttManager(IApplicantService applicantserver)
+        {
+            _applicantService = applicantserver;
+        }
+        public void GiveMask(Person person)
+        {
+
+            if (_applicantService.CheckPerson(person))
+            {
+                Console.WriteLine(person.FirstName + " için maske verildi");
+            }
+            else
+            {
+                Console.WriteLine(person.FirstName + " için maske verilemedi");
+            }
+        }
+    }
+}
---
+++ Workaround/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using Entities.Concrete;
+using System;
+
+namespace Workaround
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person1 = new Person();
+            person1.FirstName = "Aykut";
+            person1.LastName = "Şahin";
+            person1.NationalIdentity = 36585263252;
+            person1.DateOfBirth = 1998;
+            PttManager pttManager = new PttManager(new PersonManager());
+            pttManager.GiveMask(person1);
+            Console.ReadLine();
+        }
+    }
+}
